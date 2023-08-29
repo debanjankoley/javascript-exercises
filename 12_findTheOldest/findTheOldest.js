c3bf2504a14d375c258d6cc455f18d326efeca20
Debanjan Koley <81432461+debanjankoley@users.noreply.github.com>
2023-08-29 10:42:27 +0530

    finished exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(item => {              // this adds yearOfDeath if it is missing
+        if (!("yearOfDeath" in item)) {
+            item.yearOfDeath = new Date().getFullYear();;
+        }
+    })
 
+    let newPeopleArray = people.map(person => {
+        return {name : person.name, age : person.yearOfDeath - person.yearOfBirth} 
+    })
+    newPeopleArray.sort((a, b) => (a.age > b.age) ? -1 : 1);
+    return newPeopleArray[0];
 };
 
 // Do not edit below this line
